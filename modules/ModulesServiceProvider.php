fd1b24abaa111f96f735ba199575872c3e116062
jiorpilla <jiorpilla@gmail.com>
2024-05-31 05:02:49 +0800

    TODO:
    todo : implement this later on so that /bootstrap/providers.php will only need to point into here, to access all relevant Service provider from modules

---
+++ modules/ModulesServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModulesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
---
+++ modules/providers.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @TODO : think of a way to include this file on bootstrap/providers.php
+ * @todo : onhold for now..
+ */
+return [
+    Modules\Admin\Providers\UserServiceProvider::class,
+    Modules\Main\Providers\MainServiceProvider::class,
+];
